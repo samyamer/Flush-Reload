c76961a67b0e845eea249b3cfc6051dd6b669bfe
samy <samyamer200@gmail.com>
2023-11-15 21:35:58 -0500

    .

--- flush_reload.c
+++ flush_reload.c
@@ -35,7 +35,7 @@ int probe(char* addr){
     return time < THRESHOLD;
 }
 void busy_wait(){
-    for(int i =0; i<80000; i++){
+    for(int i =0; i<70000; i++){
          asm __volatile__(
         "nop\n\t"
         );
