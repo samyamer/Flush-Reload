b2d2f0f761504b0c085589ab21fee534558e747b
samyamer <SAMYAMER2000@GMAIL.COM>
2023-11-26 21:31:13 -0500

    analyze

--- analyze.py
+++ analyze.py
@@ -1,9 +1,9 @@
 import pylcs
 class Access:
    def  __init__(self, access_times):
-       self.sqr = access_times[0]<260
-       self.mul = access_times[1]<260
-       self.div = access_times[2]<260
+       self.sqr = access_times[0]<200
+       self.mul = access_times[1]<200
+       self.div = access_times[2]<200
        self.values = [access_times[0],access_times[1],access_times[2]]
 
 
@@ -21,10 +21,7 @@ def read_file(file):
 
     return [Access(i) for i in access_list]
 
-def check_list(arr):
-    spy = "".join(arr)
 
-    return (spy in d_p) or (spy in d_q)
 
 def _to_binary(time_slots):
     # We're building a state machine here
@@ -58,14 +55,14 @@ def _to_binary(time_slots):
 
         empty = False
         if(not square and not multiply and not div):
-            print(f"All miss at {end}")
+            # print(f"All miss at {end}")
             all_miss+=1
             total_all_miss +=1
             empty = True
         else:
             all_miss = 0
 
-        # If 4 or more continous all misses, this is probably a gap
+        # If 10 or more continous all misses, this is probably a gap
         if(all_miss >=10):
             output.append("?")
             current_state = START
@@ -82,10 +79,10 @@ def _to_binary(time_slots):
                 if(num_square > 3):
                     current_state = AFTER_SQUARE
                     num_square=0
-            # if multiply:
-            #     current_state = AFTER_MULTIPLY
-            #     saw_mul = end
-                # print("Start -> After_Square")
+            if multiply:
+                current_state = AFTER_MULTIPLY
+                saw_mul = end
+
 
         elif current_state == AFTER_SQUARE:
             if(div):
@@ -96,12 +93,14 @@ def _to_binary(time_slots):
 
 
             if multiply:
-                # current_state = AFTER_MULTIPLY
-                print(f"Multiply at {end}: {time_slot.values}")
-                print("Multiply after square!!")
+                current_state = AFTER_MULTIPLY
+                # print(f"Multiply at {end}: {time_slot.values}")
+                # print("Multiply after square!!")
                 saw_mul = end
 
         elif current_state == AFTER_SQUARE_MOD:
+            if div:
+                num_div +=1
 
             if multiply:
                 current_state = AFTER_MULTIPLY
@@ -112,14 +111,15 @@ def _to_binary(time_slots):
             # If we see a square, return to start and output a 0
             elif square:
                 num_square +=1
-                if(num_square > 3):
-                    current_state = AFTER_SQUARE
-                    num_square=0
-                    current_state = START
-                    output.append('0')
-                    slots.append((start,end,0))
-                    start = end+1
-                    zero_count+=1
+            if(num_square > 3 or (num_square>=1 and all_miss>3) ):
+                current_state = AFTER_SQUARE
+                num_square=0
+                num_div=0
+                current_state = START
+                output.append('0')
+                slots.append((start,end,0))
+                start = end+1
+                zero_count+=1
 
 
         elif current_state == AFTER_MULTIPLY:
@@ -127,13 +127,13 @@ def _to_binary(time_slots):
             # Alternatively, a missed slot could be a modulo
             if div:
                 num_div +=1
-                if(num_div >=3):
-                    num_div=0
-                    current_state = START
-                    output.append('1')
-                    slots.append((start,end,saw_mul,1))
-                    start = end+1
-                    one_count +=1
+            if(num_div >=2 or all_miss>3):
+                num_div=0
+                current_state = START
+                output.append('1')
+                slots.append((start,end,saw_mul,1))
+                start = end+1
+                one_count +=1
 
 
         end+=1
@@ -143,17 +143,17 @@ def _to_binary(time_slots):
             #     current_state = START
             #     output.append('_')
 
-    print(f"num 0s {zero_count}")
-    print(f"num 1s {one_count}")
-    print(slots)
-    print(f"all miss: {total_all_miss}")
-    return output
+    # print(f"num 0s {zero_count}")
+    # print(f"num 1s {one_count}")
+    # # print(slots)
+    # print(f"all miss: {total_all_miss}")
+    return output,slots
 
 
 access_list = read_file("slots_out4.txt")
 
-out = _to_binary(access_list)
-print(out)
+out,slots = _to_binary(access_list)
+# print(out)
 #print(_to_binary(read_file("slots_out3.txt")))
 
 
@@ -170,11 +170,13 @@ for char in out:
         list_out.append(element)
         prev_not_q = False
         element = []
-
+list_out.append(element)
+# print(list_out)
 lenghs = [len(e) for e in list_out]
+print(slots)
 
-print(list_out)
-print(f"longest continous: {max(lenghs)}")
+# print(list_out)
+# print(f"longest continous: {max(lenghs)}")
 spy_out ="".join(out)
 
 d_p = "101110001111001001111101000101101101110111101110001000011010010010110101011101001101011010010001001110010000101010010001011011000110100011000011000011011001000111110011101000100100100101000100010001101100011001010110110111011111111101010101101101111010111011001001011011010011100100100011101110011101101011101110010001010101011000011000100000101010010110000101011100110000001011111100000000111001100111011111111111111111001001111101010101001101010110111000010000100111100011111011101100011001111100100000110110111011100010110111100110110111010111000010110110101110111001000100000001010000100010111001011100111111100010001001111110100010000011110110000111101100001110010001010110011100110111010111100010101010001000100100011100110110110010010011101011001101000000110001100101000000111110000111011000101001111000001010111111100100100011000101100000111000111101001100110001101100110000110111010011000011111010111010101101011001111110101010101000011010010011000110000001101110010101010110000011001111011101110001001111011100100011011010010000001100100100111100001000011010100111001101110100111011101011111101010000010010001100110001110000001000001000010111000101001111101010100010011010101101010100100000001011101001100101100011101000011110011010110110010101011011011111111111101001111101100100111110111001111101001110011011001011011001001010010000010101010011100000011010010011111111010101011101111110011000101101110111011100000000010111001110001100001101011100011100011100011100001110011011000101110001010001001001011000100110010100101101111101011011010010111110011110111110001101110110100010010101111001010000101101100100110111011101101100001001001010111100111110110001100011111110001101011011000011001011101111111101001100010001011100010101101110001110000000011111111001111001010111011101101101110011011010000001111110001110110110111111000100001010010011111110010000001101111111011001010011111011101110010101110010111111000010010101001001110001001110111010110110010001000100001010111010111001110110101000010101100000111100011111000100111011101001"
@@ -184,12 +186,50 @@ d_q = "1000000110001100000000111100111010110100111011101000011011110000010010000
 
 def check_list(arr):
     spy = "".join(arr)
+    print(f"len of string is {len(spy)}")
+    print(f"lcs with dp is {pylcs.lcs_string_length(spy, d_p)}")
 
     return (spy in d_p) or (spy in d_q)
 
-exist = [check_list(e) for e in list_out]
 
-print(exist)
+
+
+# Focus on last element in the list_out
+best = "".join(list_out[len(list_out)-1])
+check_list(list_out[len(list_out)-1])
+lcs_indices = pylcs.lcs_string_idx(d_p, best) # indices of spy out
+d_p_indices = pylcs.lcs_string_idx(best, d_p) #Indices of d_p
+
+# get the strings after
+print("LASSSSST")
+
+
+spy_after = best[max(lcs_indices)+1:]
+d_p_after = d_p[max(d_p_indices)+1:]
+
+print(spy_after)
+print(f"length of spy after {len(spy_after)}")
+print("------------------------------")
+print(d_p_after)
+
+print(f"LCS of after the 69 {pylcs.lcs_string_length(spy_after, d_p_after)}")
+
+
+######################################################################################
+
+spy_lcs_indices = pylcs.lcs_string_idx(d_p, spy_out)
+
+print("-----LCS DATA------")
+print(f"Len of slots {len(slots)}")
+for i in spy_lcs_indices:
+    if(i==-1):
+        continue
+    print(slots[i])
+print()
+
+# exist = [check_list(e) for e in list_out]
+
+# print(exist)
 print(lenghs)
 
 
